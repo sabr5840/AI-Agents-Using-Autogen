2cfbeeb87348fc5ced8fd667ae21cf896ad99a1a
sabr5840 <sabr5840@stud.kea.dk>
2025-05-13 20:21:05 +0200

    config rettet

--- config.py
+++ config.py
@@ -9,13 +9,8 @@ LLM_CONFIG = {
             "model": "mistral-large-latest",  #open-mistral-nemo
             "api_key": os.getenv("MISTRAL_API_KEY"),
             "api_type": "mistral",
-            "api_rate_limit": 0.25,
-            "repeat_penalty": 1.1,
             "temperature": 0.0,
-            "seed": 42,
             "stream": False,
-            "cache_seed": None,
-            "native_tool_calls": True,
             "tool_choice": "auto",
         }
     ]
